27f701a9516e21979cfa06b05383550a044361e4
jauy2310 <jauy2310@colorado.edu>
2025-03-18 23:49:13 -0400

    Fixing

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -11,7 +11,14 @@ AESD_CHAR_GIT_SUBMODULES = YES
 
 AESD_CHAR_MODULE_SUBDIRS += aesd-char-driver
 
-AESD_CHAR_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
+AESD_CHAR_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR) ARCH=$(KERNEL_ARCH)
+
+define AESD_CHAR_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/etc/init.d/
+
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
